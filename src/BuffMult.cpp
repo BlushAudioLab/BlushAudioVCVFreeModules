694e8005dfac3475cd7dc3215de6a855497b8e53
Blush Audio Dev <blushaudio@gmail.com>
2022-09-08 19:00:36 +0100

    Buff Mult: Refactor I/O

--- src/BuffMult.cpp
+++ src/BuffMult.cpp
@@ -5,17 +5,17 @@ struct BuffMult : Module {
 		PARAMS_LEN
 	};
 	enum InputId {
-		AUDIOCVIN1_INPUT,
-		AUDIOCVIN2_INPUT,
+		IN1_INPUT,
+		IN2_INPUT,
 		INPUTS_LEN
 	};
 	enum OutputId {
-		AUDIOCVOUT1_OUTPUT,
-		AUDIOCVOUT2_OUTPUT,
-		AUDIOCVOUT3_OUTPUT,
-		AUDIOCVOUT4_OUTPUT,
-		AUDIOCVOUT5_OUTPUT,
-		AUDIOCVOUT6_OUTPUT,
+		OUT1_OUTPUT,
+		OUT2_OUTPUT,
+		OUT3_OUTPUT,
+		OUT4_OUTPUT,
+		OUT5_OUTPUT,
+		OUT6_OUTPUT,
 		OUTPUTS_LEN
 	};
 	enum LightId {
@@ -24,43 +24,43 @@ struct BuffMult : Module {
 
 	BuffMult() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configInput(AUDIOCVIN1_INPUT, "");
-		configInput(AUDIOCVIN2_INPUT, "");
-		configOutput(AUDIOCVOUT1_OUTPUT, "");
-		configOutput(AUDIOCVOUT2_OUTPUT, "");
-		configOutput(AUDIOCVOUT3_OUTPUT, "");
-		configOutput(AUDIOCVOUT4_OUTPUT, "");
-		configOutput(AUDIOCVOUT5_OUTPUT, "");
-		configOutput(AUDIOCVOUT6_OUTPUT, "");
+		configInput(IN1_INPUT, "Input 1");
+		configInput(IN2_INPUT, "Input 2");
+		configOutput(OUT1_OUTPUT, "Output 1");
+		configOutput(OUT2_OUTPUT, "Output 2");
+		configOutput(OUT3_OUTPUT, "Output 3");
+		configOutput(OUT4_OUTPUT, "Output 4");
+		configOutput(OUT5_OUTPUT, "Output 5");
+		configOutput(OUT6_OUTPUT, "Output 6");
 	}
 
 	void process(const ProcessArgs& args) override {
 
 		//setting up the input
-		int n = inputs[AUDIOCVIN1_INPUT].getChannels();
-		float* out = inputs[AUDIOCVIN1_INPUT].getVoltages();
+		int in = inputs[IN1_INPUT].getChannels();
+		float* out = inputs[IN1_INPUT].getVoltages();
 
 		//routing input to outputs 1,2,3
-		outputs[AUDIOCVOUT1_OUTPUT].setChannels(n);
-		outputs[AUDIOCVOUT1_OUTPUT].writeVoltages(out);
-		outputs[AUDIOCVOUT2_OUTPUT].setChannels(n);
-		outputs[AUDIOCVOUT2_OUTPUT].writeVoltages(out);
-		outputs[AUDIOCVOUT3_OUTPUT].setChannels(n);
-		outputs[AUDIOCVOUT3_OUTPUT].writeVoltages(out);
+		outputs[OUT1_OUTPUT].setChannels(in);
+		outputs[OUT1_OUTPUT].writeVoltages(out);
+		outputs[OUT2_OUTPUT].setChannels(in);
+		outputs[OUT2_OUTPUT].writeVoltages(out);
+		outputs[OUT3_OUTPUT].setChannels(in);
+		outputs[OUT3_OUTPUT].writeVoltages(out);
 
 		//statement taking care of "normalling" behaviour for all outputs below this - i.e 4,5,6
-		if (inputs[AUDIOCVIN2_INPUT].isConnected()){
-			n = inputs[AUDIOCVIN2_INPUT].getChannels();
-			out = inputs[AUDIOCVIN2_INPUT].getVoltages();	
+		if (inputs[IN2_INPUT].isConnected()){
+			in = inputs[IN2_INPUT].getChannels();
+			out = inputs[IN2_INPUT].getVoltages();	
 		}
 
 		//routing input to ouputs 4,5,6 "normalled" behaviour from Input 1 when Input 2 not connected, using Input 2 when connected
-		outputs[AUDIOCVOUT4_OUTPUT].setChannels(n);
-		outputs[AUDIOCVOUT4_OUTPUT].writeVoltages(out);
-		outputs[AUDIOCVOUT5_OUTPUT].setChannels(n);
-		outputs[AUDIOCVOUT5_OUTPUT].writeVoltages(out);
-		outputs[AUDIOCVOUT6_OUTPUT].setChannels(n);
-		outputs[AUDIOCVOUT6_OUTPUT].writeVoltages(out);
+		outputs[OUT4_OUTPUT].setChannels(in);
+		outputs[OUT4_OUTPUT].writeVoltages(out);
+		outputs[OUT5_OUTPUT].setChannels(in);
+		outputs[OUT5_OUTPUT].writeVoltages(out);
+		outputs[OUT6_OUTPUT].setChannels(in);
+		outputs[OUT6_OUTPUT].writeVoltages(out);
 	}
 };
 
@@ -74,15 +74,15 @@ struct BuffMultWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 18.73)), module, BuffMult::AUDIOCVIN1_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 69.6)), module, BuffMult::AUDIOCVIN2_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 18.73)), module, BuffMult::IN1_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 69.6)), module, BuffMult::IN2_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 31.5)), module, BuffMult::AUDIOCVOUT1_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 44.1)), module, BuffMult::AUDIOCVOUT2_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 56.9)), module, BuffMult::AUDIOCVOUT3_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 82.3)), module, BuffMult::AUDIOCVOUT4_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 95)), module, BuffMult::AUDIOCVOUT5_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 107.8)), module, BuffMult::AUDIOCVOUT6_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 31.5)), module, BuffMult::OUT1_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 44.1)), module, BuffMult::OUT2_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 56.9)), module, BuffMult::OUT3_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 82.3)), module, BuffMult::OUT4_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 95)), module, BuffMult::OUT5_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 107.8)), module, BuffMult::OUT6_OUTPUT));
 	}
 };
 
