6f72a2051084c68139d269ef629e8ff74bd053ad
Blush Audio Dev <blushaudio@gmail.com>
2022-10-11 17:53:19 +0100

    Coloratura: Basic I/O and Panel

--- src/Coloratura.cpp
+++ src/Coloratura.cpp
@@ -1,34 +1,30 @@
 #include "plugin.hpp"
 
-
 struct Coloratura : Module {
 	enum ParamId {
 		PARAMS_LEN
 	};
 	enum InputId {
-		PITCH_INPUT,
+		CHORUS_INPUT,
 		INPUTS_LEN
 	};
 	enum OutputId {
-		SINE_OUTPUT,
+		CHORUS_OUTPUT,
 		OUTPUTS_LEN
 	};
 	enum LightId {
-		BLINK_LIGHT,
-		BLINK_LIGHT,
 		LIGHTS_LEN
 	};
 
 	Coloratura() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configInput(PITCH_INPUT, "");
-		configOutput(SINE_OUTPUT, "");
+		configInput(CHORUS_INPUT, "Input");
+		configOutput(CHORUS_OUTPUT, "Output");
 	}
 
 	void process(const ProcessArgs& args) override {
 	}
 };
-
 
 struct ColoraturaWidget : ModuleWidget {
 	ColoraturaWidget(Coloratura* module) {
@@ -40,13 +36,10 @@ struct ColoraturaWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8.89, 19.269)), module, Coloratura::PITCH_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8.89, 27.619)), module, Coloratura::CHORUS_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.89, 36.794)), module, Coloratura::SINE_OUTPUT));
-
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(22.182, 36.794)), module, Coloratura::BLINK_LIGHT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.89, 103.586)), module, Coloratura::CHORUS_OUTPUT));
 	}
 };
-
 
 Model* modelColoratura = createModel<Coloratura, ColoraturaWidget>("Coloratura");
