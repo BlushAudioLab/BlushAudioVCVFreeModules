a8de6436c24c3c74a821ae2a6af12304c61f7eb7
Blush Audio Dev <blushaudio@gmail.com>
2022-10-11 19:08:09 +0100

    Coloratura: Some Parameter Set-Up
    Also added .hpp file - can put stuff in there later

--- src/Coloratura.cpp
+++ src/Coloratura.cpp
@@ -1,7 +1,11 @@
 #include "plugin.hpp"
+#include "Coloratura.hpp"
+#include <samplerate.h>
 
 struct Coloratura : Module {
 	enum ParamId {
+		DELAY_PARAM, //delay time
+		REGEN_PARAM, //feedback amount
 		PARAMS_LEN
 	};
 	enum InputId {
@@ -16,13 +20,40 @@ struct Coloratura : Module {
 		LIGHTS_LEN
 	};
 
+	constexpr static size_t HISTORY_SIZE = 1 << 21;
+	dsp::DoubleRingBuffer<float, HISTORY_SIZE> historyBuffer;
+	dsp::DoubleRingBuffer<float, 16> outBuffer;
+	SRC_STATE* src;
+	float lastWet = 0.f;
+	float clockFreq = 1.f;
+	dsp::Timer clockTimer;
+	dsp::SchmittTrigger clockTrigger;
+	float clockPhase = 0.f;
+
+	//constructor
 	Coloratura() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+
+		const float delayMin = log10(0.001f * 1000) / 4;
+		const float delayMax = log10(10.f * 1000) / 4;
+		const float delayDefault = log10(0.5f * 1000) / 4;
+
+		configParam(DELAY_PARAM, delayMin, delayMax, delayDefault, "Delay", " s", 10.f / 1e-3, 1e-3);
+		configParam(REGEN_PARAM, 0.f, 1.f, 0.5f, "Feedback", "%", 0, 100 );
+
 		configInput(CHORUS_INPUT, "Input");
 		configOutput(CHORUS_OUTPUT, "Output");
+
+		src = src_new(SRC_SINC_FASTEST, 1, NULL);
+	}
+
+	//destructor
+	~Coloratura() {
+		src_delete(src); //probably deleting some memory?
 	}
 
 	void process(const ProcessArgs& args) override {
+
 	}
 };
 
